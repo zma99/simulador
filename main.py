914951e4777ff043c7458ff4891211cc855b7d92
SebasRomero97 <114784453+SebasRomero97@users.noreply.github.com>
2022-10-23 05:01:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-from interfaz import *
 
+from clases import *
 
+OPCIONES_MENU ={
+    '1':'Cargar procesos manualmente',
+    '2':'Cargar procesos desde archivo',
+    '3':'Acerca del Simulador',
+    '4':'Salir'
+}
 
+if __name__ == '__main__':
+    ventana = Consola()
+    ventana.limpiar()
+    menu = Menu(OPCIONES_MENU)
+    menu.mostrar()
+    datos_procesos = menu.capturar()
+    encabezados = datos_procesos.pop(0)
+    tabla_planificacion = Tabla('1', encabezados, datos_procesos)
+    ventana.limpiar()
+    print('Planificación de procesos:')
+    tabla_planificacion.construir()
 
-#seba
+    # Praparando memoria
+    datos_particiones = [12,25,40,100]
+    #print(f'Las particiones se crearán con los siguiente datos: {datos_particiones}\n\n')
+    memo = Memoria(datos_particiones)
+    #print(memo.particiones[0].getId())
+
+    datos_particiones = list()
+
+    for part in memo.particiones:
+        particion = list()
+        particion.append(part.getId())
+        particion.append(part.getDirInicio())
+        particion.append(part.getTam())
+        datos_particiones.append(particion)
+
+    encabezados =['Num', 'Dir inicio', 'Tamaño (KB)']
+    monitor_memo = Tabla('monitor_memo', encabezados, datos_particiones)
+    print('\nDistribución de particiones en MP:')
+    monitor_memo.construir()
+
+    print('Cantidad de particiones: ', memo.getCantPart())
+    print(f'Tamaño total de la memoria: {memo.getTam()} KB')
